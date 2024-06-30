3a03febeb2877a64a10b89daa9e6094506501fa0
arimateiajunior.tic@gmail.com <arimateiajunior.tic@gmail.com>
2024-06-30 12:15:59 -0300

    Adicionando métricas para o Prometheus

--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, request, jsonify
 from prometheus_client import Counter, generate_latest, REGISTRY, Histogram, Gauge
 import time
+import logging
 
 main = Blueprint("main", __name__)
+
+# Configuração de logging
+logging.basicConfig(level=logging.DEBUG)
 
 # Exemplo de métrica de contador
 requests_total = Counter('requests_total', 'Total de requisições recebidas pelo endpoint', ['endpoint'])
@@ -29,6 +33,7 @@ def metrics():
 def calcular():
     start_time = time.time()
     in_progress_requests.labels('/calcular').inc()
+    logging.debug(f"Increment in /calcular: {in_progress_requests.labels('/calcular')._value.get()}")
     try:
         data = request.json
         idade_grupo = data.get('idade_grupo')
@@ -51,18 +56,22 @@ def calcular():
         return response
     finally:
         in_progress_requests.labels('/calcular').dec()
+        logging.debug(f"Decrement in /calcular: {in_progress_requests.labels('/calcular')._value.get()}")
 
 # Adicionando middleware para medir requisições em andamento
 @main.before_request
 def before_request():
     in_progress_requests.labels(request.path).inc()
+    logging.debug(f"Increment in before_request: {in_progress_requests.labels(request.path)._value.get()}")
 
 @main.after_request
 def after_request(response):
     in_progress_requests.labels(request.path).dec()
+    logging.debug(f"Decrement in after_request: {in_progress_requests.labels(request.path)._value.get()}")
     return response
 
 @main.teardown_request
 def teardown_request(exception):
     in_progress_requests.labels(request.path).dec()
+    logging.debug(f"Decrement in teardown_request: {in_progress_requests.labels(request.path)._value.get()}")
     return None
